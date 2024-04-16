d95a0610289b222a678f34b42a84252fe25c9bf3
andreLake <andre.lake@hotmail.com>
2024-04-15 22:21:07 -0300

    created

---
+++ src/main/java/com/andrelake/javacodeproblems/JavaCodeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.andrelake.javacodeproblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrelake/javacodeproblems/JavaCodeProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelake.javacodeproblems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
